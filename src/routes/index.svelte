20ce9e1abae6ef6991370aea7b1f03e07ee817c6
Ncookiez <ncukiert@gmail.com>
2022-07-07 12:29:23 -0300

    Hiding Optimism for now

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -48,7 +48,7 @@
 	<span class="eth" class:selected={selectedChain === 'eth'} on:click={() => selectedChain = 'eth'}><img src="/images/ethereum.svg" alt="ETH"></span>
 	<span class="poly" class:selected={selectedChain === 'poly'} on:click={() => selectedChain = 'poly'}><img src="/images/polygon.svg" alt="POLY"></span>
 	<span class="avax" class:selected={selectedChain === 'avax'} on:click={() => selectedChain = 'avax'}><img src="/images/avalanche.svg" alt="AVAX"></span>
-	<span class="op" class:selected={selectedChain === 'op'} on:click={() => selectedChain = 'op'}><img src="/images/optimism.svg" alt="OP"></span>
+	<!-- <span class="op" class:selected={selectedChain === 'op'} on:click={() => selectedChain = 'op'}><img src="/images/optimism.svg" alt="OP"></span> -->
 </div>
 
 <!-- Quick Navigation -->
@@ -112,13 +112,14 @@
 	}
 
 	div.chainSelector span.avax {
+		border-radius: 0 1em 1em 0;
 		background-color: #E84142;
 	}
 
-	div.chainSelector span.op {
+	/* div.chainSelector span.op {
 		border-radius: 0 1em 1em 0;
 		background-color: #FF0420;
-	}
+	} */
 
 	div.chainSelector span.selected {
 		outline: 2px solid var(--secondary-color);
