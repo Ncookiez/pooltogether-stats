eb33ca60a61b355c305f5fdac54b701186388cd0
Ncookiez <ncukiert@gmail.com>
2022-07-11 15:58:48 -0300

    Added Supply Query & Updated Snapshot

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 
 // Imports:
-import { prizePoolABI, prizeDistributorABI, ticketABI, flushABI } from './ABIs.js';
-import { queryBlocks, parseBN, multicallOneContractQuery } from 'weaverfi/dist/functions.js';
+import { prizePoolABI, prizeDistributorABI, ticketABI, flushABI, aaveUSDCABI } from './ABIs.js';
+import { query, queryBlocks, parseBN, multicallOneContractQuery } from 'weaverfi/dist/functions.js';
 import { getChainName, writeJSON, readJSON, getLatestBlock, getCurrentBlock } from './functions.js';
 
 // Ethereum Contract Addresses:
@@ -9,24 +9,32 @@ const ethPrizePool = '0xd89a09084555a7D0ABe7B111b1f78DFEdDd638Be';
 const ethPrizeDistributor = '0xb9a179DcA5a7bf5f8B9E088437B3A85ebB495eFe';
 const ethTicket = '0xdd4d117723C257CEe402285D3aCF218E9A8236E1';
 const ethFlush = '0x2193b28b2BdfBf805506C9D91Ed2021bA6fBc888';
+const ethAaveUSDC = '0xBcca60bB61934080951369a648Fb03DF4F96263C';
+const ethYieldSource = '0x32e8D4c9d1B711BC958d0Ce8D14b41F77Bb03a64';
 
 // Polygon Contract Addresses:
 const polyPrizePool = '0x19DE635fb3678D8B8154E37d8C9Cdf182Fe84E60';
 const polyPrizeDistributor = '0x8141BcFBcEE654c5dE17C4e2B2AF26B67f9B9056';
 const polyTicket = '0x6a304dFdb9f808741244b6bfEe65ca7B3b3A6076';
 const polyFlush = '0xA2342489470474536F04cd4DdA2e8658303b305d';
+const polyAaveUSDC = '0x1a13F4Ca1d028320A707D99520AbFefca3998b7F';
+const polyYieldSource = '0xD4F6d570133401079D213EcF4A14FA0B4bfB5b9C';
 
 // Avalanche Contract Addresses:
 const avaxPrizePool = '0xF830F5Cb2422d555EC34178E27094a816c8F95EC';
 const avaxPrizeDistributor = '0x83332F908f403ce795D90f677cE3f382FE73f3D1';
 const avaxTicket = '0xB27f379C050f6eD0973A01667458af6eCeBc1d90';
 const avaxFlush = '0x1B20994C3894EcC862e26A9F4EC626A8489DD051';
+const avaxAaveUSDC = '0x46A51127C3ce23fb7AB1DE06226147F446e4a857';
+const avaxYieldSource = '0x7437db21A0dEB844Fa64223e2d6Db569De9648Ff';
 
 // Optimism Contract Addresses:
 const opPrizePool = '0x79Bc8bD53244bC8a9C8c27509a2d573650A83373';
 const opPrizeDistributor = '0x722e9BFC008358aC2d445a8d892cF7b62B550F3F';
 const opTicket = '0x62BB4fc73094c83B5e952C2180B23fA7054954c4';
 const opFlush = '0x4c65F496B78b7E81c15723f56a43925E5dc3a0e1';
+const opAaveUSDC = '0x625E7708f30cA75bfd92586e17077590C60eb4cD';
+const opYieldSource = '0x4ecB5300D9ec6BCA09d66bfd8Dcb532e3192dDA1';
 
 /* ====================================================================================================================================================== */
 
@@ -49,6 +57,7 @@ const executeQueries = async () => {
     await queryClaims(chain);
     await queryWallets(chain);
     await queryYield(chain);
+    await querySupply(chain);
   })());
   await Promise.all(promises);
 
@@ -304,6 +313,45 @@ const queryYield = async (chain) => {
     });
     writeJSON(yieldCaptures, fileName);
   }
+}
+
+/* ====================================================================================================================================================== */
+
+// Function to query for supply values on a specific chain:
+const querySupply = async (chain) => {
+
+  // Initializations:
+  const fileName = `${chain}/supply`;
+  const chainName = getChainName(chain);
+  const decimals = 6;
+  let supply = {
+    aToken: 0,
+    ticket: 0
+  }
+
+  // Querying Aave Supply:
+  if(chain === 'eth') {
+    supply.aToken = parseInt(await query(chain, ethAaveUSDC, aaveUSDCABI, 'balanceOf', [ethYieldSource])) / (10 ** decimals);
+  } else if(chain === 'poly') {
+    supply.aToken = parseInt(await query(chain, polyAaveUSDC, aaveUSDCABI, 'balanceOf', [polyYieldSource])) / (10 ** decimals);
+  } else if(chain === 'avax') {
+    supply.aToken = parseInt(await query(chain, avaxAaveUSDC, aaveUSDCABI, 'balanceOf', [avaxYieldSource])) / (10 ** decimals);
+  } else if(chain === 'op') {
+    supply.aToken = parseInt(await query(chain, opAaveUSDC, aaveUSDCABI, 'balanceOf', [opYieldSource])) / (10 ** decimals);
+  }
+
+  // Querying Ticket Supply:
+  if(chain === 'eth') {
+    supply.ticket = parseInt(await query(chain, ethTicket, ticketABI, 'totalSupply', [])) / (10 ** decimals);
+  } else if(chain === 'poly') {
+    supply.ticket = parseInt(await query(chain, polyTicket, ticketABI, 'totalSupply', [])) / (10 ** decimals);
+  } else if(chain === 'avax') {
+    supply.ticket = parseInt(await query(chain, avaxTicket, ticketABI, 'totalSupply', [])) / (10 ** decimals);
+  } else if(chain === 'op') {
+    supply.ticket = parseInt(await query(chain, opTicket, ticketABI, 'totalSupply', [])) / (10 ** decimals);
+  }
+  console.log(`  > ${chainName}: Queried token supplies.`);
+  writeJSON([supply], fileName);
 }
 
 /* ====================================================================================================================================================== */
