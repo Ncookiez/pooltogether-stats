a0854833a184ecb7091706539671ef74d6a23a1d
Ncookiez <ncukiert@gmail.com>
2022-07-11 16:38:25 -0300

    Added Yield Deficit Chart

--- src/lib/Yield.svelte
+++ src/lib/Yield.svelte
@@ -18,19 +18,24 @@
 
 	// Charts:
 	let cumulativeYieldAmountsChart: Chart;
+	let yieldVsClaimAmountsChart: Chart;
 
 	// JSON Files:
 	let yieldOverTime: { timestamps: number[], yieldAmounts: number[], yieldCounts: number[], cumulativeYieldAmounts: number[], cumulativeYieldCounts: number[] }[] | undefined;
 	let supply: { aToken: number, ticket: number } | undefined;
+	let claimsOverTime: { timestamps: number[], claimAmounts: number[], claimCounts: number[], prizeCounts: number[], avgClaimAmounts: number[], cumulativeClaimAmounts: number[], cumulativeClaimCounts: number[], cumulativePrizeCounts: number[] }[] | undefined;
 
 	// Reactive Data:
 	$: getYieldOverTime(selectedChain);
 	$: getSupplyData(selectedChain);
+	$: getClaimsOverTime(selectedChain);
 	$: timestamps = yieldOverTime ? yieldOverTime[0].timestamps.map(time => (new Date(time * 1000)).toLocaleString(undefined, { month: 'short', day: 'numeric', year: 'numeric' })) : [];
 	$: totalYieldAmount = yieldOverTime ? yieldOverTime[0].cumulativeYieldAmounts[yieldOverTime[0].cumulativeYieldAmounts.length - 1] : 0;
+	$: totalClaimAmount = claimsOverTime ? claimsOverTime[0].cumulativeClaimAmounts[claimsOverTime[0].cumulativeClaimAmounts.length - 1] : 0;
 
 	// Reactive Chart Data:
 	$: yieldOverTime, setCumulativeYieldAmountsChartData();
+	$: yieldOverTime, claimsOverTime, setYieldVsClaimAmountsChartData();
 
 	// Function to find appropriate yield over time data:
 	const getYieldOverTime = async (chain: 'eth' | 'poly' | 'avax' | 'op') => {
@@ -40,6 +45,11 @@
 	// Function to find appropriate supply data:
 	const getSupplyData = async (chain: 'eth' | 'poly' | 'avax' | 'op') => {
 		supply = (await import(`./data/${chain}/supply.json`)).default[0];
+	}
+
+	// Function to find appropriate claims over time data:
+	const getClaimsOverTime = async (chain: 'eth' | 'poly' | 'avax' | 'op') => {
+		claimsOverTime = (await import(`./data/${chain}/claimsOverTime.json`)).default;
 	}
 
 	// Function to set cumulative yield amounts chart data:
@@ -64,6 +74,40 @@
 		}
 	}
 
+	// Function to set yield vs claim amounts chart data:
+	const setYieldVsClaimAmountsChartData = () => {
+		if(yieldVsClaimAmountsChart && yieldOverTime && claimsOverTime) {
+			yieldVsClaimAmountsChart.data.labels = timestamps;
+			yieldVsClaimAmountsChart.data.datasets = [
+				{
+					label: 'Yield Amount',
+					data: yieldOverTime[0].cumulativeYieldAmounts,
+					backgroundColor: backgroundColor,
+					borderColor: lineColor,
+					borderWidth: lineWidth,
+					pointRadius: pointSize,
+					pointHoverRadius: pointHoverSize,
+					tension: lineTension
+				},
+				{
+					label: 'Claim Amount',
+					data: claimsOverTime[0].cumulativeClaimAmounts,
+					backgroundColor: lineColor,
+					borderColor: backgroundColor,
+					borderWidth: lineWidth,
+					pointRadius: pointSize,
+					pointHoverRadius: pointHoverSize,
+					tension: lineTension
+				}
+			];
+			if(yieldVsClaimAmountsChart.options.plugins?.tooltip && yieldVsClaimAmountsChart.options.scales?.y && yieldVsClaimAmountsChart.options.scales.y.ticks) {
+				yieldVsClaimAmountsChart.options.plugins.tooltip.callbacks = { label: (item) => { return `${item.dataset.label}: $${(item.raw as number).toLocaleString(undefined)}` } };
+				yieldVsClaimAmountsChart.options.scales.y.ticks.callback = (value) => { return '$' + value.toLocaleString(undefined) };
+			}
+			yieldVsClaimAmountsChart.update();
+		}
+	}
+
 	onMount(() => {
 
 		// Chart.js Registrations:
@@ -71,9 +115,11 @@
 
 		// Initializing Charts:
 		cumulativeYieldAmountsChart = new Chart('cumulativeYieldAmountsChart', structuredClone(lineChartConfig));
+		yieldVsClaimAmountsChart = new Chart('yieldVsClaimAmountsChart', structuredClone(lineChartConfig));
 		
 		// Setting Chart Data:
 		setCumulativeYieldAmountsChartData();
+		setYieldVsClaimAmountsChartData();
 
 	});
 	
@@ -87,14 +133,19 @@
 	<h2>Yield Stats</h2>
 
 	<!-- Cumulative Yield Amounts Over Time Chart -->
-	<span>Over <strong>${totalYieldAmount.toLocaleString(undefined)}</strong>	in yield has been captured in <strong>{getChainName(selectedChain)}</strong> thus far.</span>
+	<span><strong>${totalYieldAmount.toLocaleString(undefined)}</strong>	in yield has been captured in <strong>{getChainName(selectedChain)}</strong> thus far.</span>
 	<canvas id="cumulativeYieldAmountsChart" />
 
-	<!-- Confident Depositors -->
+	<!-- Uncaptured Yield -->
 	<div>
+		<span>What about any uncaptured yield?</span>
 		<span>As of <strong>{getTimestamp()}</strong>, there is <strong>{supply ? supply.aToken.toLocaleString(undefined, { maximumFractionDigits: 0 }) : 0} USDC</strong> deposited on Aave.</span>
 		<span>There is <strong>${supply ? (supply.aToken - supply.ticket).toLocaleString(undefined, { maximumFractionDigits: 0 }) : 0}</strong> of yield available to be claimed!</span>
 	</div>
+
+	<!-- Yield Vs Claim Amounts Chart -->
+	<span>There is a <strong>{(totalYieldAmount - totalClaimAmount) > 0 ? 'surplus' : 'deficit'}</strong> of <strong>${Math.abs(totalYieldAmount - totalClaimAmount).toLocaleString(undefined, { maximumFractionDigits: 0 })}</strong> between yield captured and prize claims.</span>
+	<canvas id="yieldVsClaimAmountsChart" />
 
 </section>
 
